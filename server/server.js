22d9fc6ea6f01166dac2fc9fa2762b31eb6de48e
Thierno Barry <thiernobarry554@gmail.com>
2021-12-05 16:05:12 +0100

    done

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 connectDatabase();
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
